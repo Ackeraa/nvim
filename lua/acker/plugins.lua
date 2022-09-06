e30dc92a51d21d76d4434dd83f7c893cd7f345cd
Ackeraa <acker2880@gmail.com>
2022-09-06 15:04:43 +0800

    fixed conflicts

--- lua/acker/plugins.lua
+++ lua/acker/plugins.lua
@@ -100,10 +100,6 @@ return packer.startup(function(use)
   -- Last Place
   use 'ethanholz/nvim-lastplace'
 
-  -- Vim Games
-  use 'ThePrimeagen/vim-be-good'
-  use '~/MyProgram/Lua/master-vim.nvim'
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
